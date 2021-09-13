2dee4d5c4c8e69425a1392851be711872dcf747a
Alif Nur Rohman Hidayat Handoko Jati <31159695+Hidayathandoko@users.noreply.github.com>
2021-09-13 12:24:22 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,11 @@ var xxx=null;
 if(x=="1"){xxx="2020/01/boost-your-grades-or-career-with-online-tuitoring.html"}
 if(x=="2"){xxx="2020/01/role-of-big-data-and-cloud-in-gaming-industry.html"}
 if(x=="3"){xxx="2020/01/the-unique-way-to-market-with-seo.html"}
-if(x=="4"){xxx="how-to-find-affordable-web-hosting.html"}a_to_ve[i].href="https://safelink.megalife.media/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="2020/01/how-to-find-affordable-web-hosting.html"}
+if(x=="5"){xxx="2021/07/how-to-factory-reset-iphone.html"}
+if(x=="6"){xxx="2020/01/how-to-find-affordable-web-hosting.html"}
+if(x=="7"){xxx="2020/01/how-to-find-affordable-web-hosting.html"}
+if(x=="8"){xxx="2020/01/how-to-find-affordable-web-hosting.html"}a_to_ve[i].href="https://safelink.megalife.media/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
